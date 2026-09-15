c73da43bbfb0a89d23882a2a7193f885b398f99f
Leigh McGuinness <leighmcguinness@hotmail.com>
2026-09-15 16:31:49 +0100

    ADD: root and first react to render

---
+++ static-page/scripts.jsx
@@ -0,0 +1,7 @@
+import { createRoot } from "react-dom/client";
+
+// 1. create root element that react code will be rendered to
+const root = createRoot(document.getElementById("root");
+
+// 2. Render React code to the root 
+root.render(<h1>Hello React</h1>);
